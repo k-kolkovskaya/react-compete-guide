b25575817fefc4b120a842f6cc4c493f8b7fa3c9
KrystsinaKalkouskaya <moszechok@gmail.com>
2019-11-22 14:37:21 +0300

    Recompose components

--- src/ValidationComponent/ValidationComponent.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const validationComponent = props => {
-  return <div>{props.length <= 5 ? <p>Too short!</p> : <p>It's ok</p>}</div>;
-};
-
-export default validationComponent;
